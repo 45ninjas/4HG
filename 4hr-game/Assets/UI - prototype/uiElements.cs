9b752134ba5c582dac98a975829b7d406cbeebf8
alexkirwan29 <alexkirwan29@gmail.com>
2019-02-17 20:46:22 +0800

    ui

---
+++ 4hr-game/Assets/UI - prototype/uiElements.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class uiElements : MonoBehaviour
+{
+
+    public string seedID;
+    public Text seed;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        seed.text = "Seed: " + seedID;
+
+    }
+}
