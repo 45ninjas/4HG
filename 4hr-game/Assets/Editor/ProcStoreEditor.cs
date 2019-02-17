7b527a15f7915f6c79f661c77c3ffd20989a22f7
Those45Ninjas <tom.parker1235@gmail.com>
2019-02-17 20:42:29 +0800

    Created a floor plan generator and editor to go with

---
+++ 4hr-game/Assets/Editor/ProcStoreEditor.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace NineFive.Proc
+{
+    [CustomEditor(typeof(Store))]
+    public class ProcStoreEditor : Editor
+    {
+        GUIStyle pic;
+
+        public override void OnInspectorGUI()
+        {
+            if (pic == null)
+            {
+                pic = new GUIStyle();
+                pic.normal.background = null;
+            }
+            Store store = (Store)target;
+
+            // Draw the standard UI.
+            EditorGUI.BeginChangeCheck();
+            base.OnInspectorGUI();
+            if (EditorGUI.EndChangeCheck())
+            {
+                PreviewStore(store);
+            }
+
+            GUILayout.BeginHorizontal(EditorStyles.toolbar);
+
+            if (GUILayout.Button("Preview", EditorStyles.toolbarButton))
+            {
+                PreviewStore(store);
+            }
+            if (GUILayout.Button("Generate", EditorStyles.toolbarButton))
+            {
+                store.CreateStore(store.StartingSeed);
+            }
+            float height = EditorGUIUtility.currentViewWidth - 21;
+
+            GUILayout.EndHorizontal();
+
+            GUILayout.Label("Preview", pic, GUILayout.ExpandWidth(true), GUILayout.Height(height));
+        }
+
+        void PreviewStore(Store store)
+        {
+            const int size = 64;
+            var preview = new Texture2D(size, size);
+            preview.filterMode = FilterMode.Point;
+            var floorPlan = new Store.FloorPlan(Store.blockSize, Store.blockMoves, new System.Random(store.StartingSeed.GetHashCode()));
+            var tiles = floorPlan.GetTiles();
+
+            for (int x = 0; x < size; x++)
+            {
+                for (int y = 0; y < size; y++)
+                {
+                    preview.SetPixel(x, y, Color.black);
+                }
+            }
+
+            foreach (var tile in tiles)
+            {
+                preview.SetPixel(tile.x + (size / 2), tile.y + (size / 2), Color.green);
+            }
+
+            preview.Apply();
+            pic.normal.background = preview;
+        }
+    }
+}
---
+++ 4hr-game/Assets/Scripts/Proc/Store.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NineFive.Proc
+{
+    public class Store : MonoBehaviour
+    {
+        public const int blockSize = 4;
+        public const int blockMoves = 4;
+
+        System.Random random;
+
+        Vector2Int[] tiles;
+
+        [SerializeField]
+        public string StartingSeed = "Hello World";
+
+        [HideInInspector]
+        public int Seed;
+
+        public void CreateStore(string seed)
+        {
+            CreateStore(seed.GetHashCode());
+        }
+        public void CreateStore()
+        {
+            int seed = new System.Random().Next();
+            CreateStore(seed);
+        }
+
+        public void CreateStore(int seed)
+        {
+            random = new System.Random(seed);
+            Seed = seed;
+
+            var floorPlan = new FloorPlan(blockSize, blockMoves, random);
+
+            tiles = floorPlan.GetTiles();
+        }
+
+        private void Start()
+        {
+            CreateStore(StartingSeed);
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            if (tiles != null)
+            {
+                foreach (var tile in tiles)
+                {
+                    Gizmos.color = Color.green;
+                    Gizmos.DrawWireCube(new Vector3(tile.x, 0, tile.y), new Vector3(1, 0.01f, 1));
+                    Gizmos.color = new Color(0, 1f, 0, 0.2f);
+                    Gizmos.DrawCube(new Vector3(tile.x, 0, tile.y), new Vector3(1, 0.01f, 1));
+                }
+            }
+        }
+
+        public class FloorPlan
+        {
+            private int blockSize = 4;
+
+            private int moves = 3;
+
+            private System.Random random;
+
+            public FloorPlan(int size, int moves, System.Random random)
+            {
+                blockSize = size;
+                this.moves = moves;
+                this.random = random;
+            }
+
+            public Vector2Int[] GetBlocks()
+            {
+                Vector2Int[] square = new Vector2Int[]
+                {
+                    new Vector2Int(0, 0),
+                    new Vector2Int(0, 1),
+                    new Vector2Int(1, 1),
+                    new Vector2Int(1, 0)
+                };
+
+                Vector2Int[] directions = new Vector2Int[]
+                {
+                    Vector2Int.up,
+                    Vector2Int.left,
+                    Vector2Int.down,
+                    Vector2Int.right
+                };
+
+                List<Vector2Int> blocks = new List<Vector2Int>(square);
+
+                Vector2Int position = new Vector2Int();
+                for (int i = 1; i < moves; i++)
+                {
+                    Vector2Int direction = new Vector2Int();
+
+                    int fails = 10;
+
+                    while (!SquareExists(position + direction, ref blocks) && fails > 0)
+                    {
+                        direction = directions[random.Next(0, 4)];
+                        fails--;
+                    }
+                    // Pick a random direction to move the position in.
+
+                    for (int j = 0; j < square.Length; j++)
+                    {
+                        if (!blocks.Contains(direction + position + square[j]))
+                            blocks.Add(direction + position + square[j]);
+                    }
+
+                    position += direction;
+                }
+
+                return blocks.ToArray();
+            }
+
+            private bool SquareExists(Vector2Int position, ref List<Vector2Int> blocks)
+            {
+                int count = 0;
+                for (int j = 0; j < 4; j++)
+                {
+                    if (!blocks.Contains(position + blocks[j]))
+                        count++;
+                }
+                return count >= 2;
+            }
+
+            public Vector2Int[] GetTiles()
+            {
+                var blocks = GetBlocks();
+
+                Vector2Int[] tiles = new Vector2Int[blocks.Length * blockSize * blockSize];
+
+                int t = 0;
+                foreach (var block in blocks)
+                {
+                    Vector2Int pos = new Vector2Int();
+                    for (pos.x = 0; pos.x < blockSize; pos.x++)
+                    {
+                        for (pos.y = 0; pos.y < blockSize; pos.y++, t++)
+                        {
+                            tiles[t] = block * blockSize + pos;
+                        }
+                    }
+                }
+
+                return tiles;
+            }
+        }
+    }
+}
