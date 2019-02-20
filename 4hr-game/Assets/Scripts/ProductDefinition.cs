ef2a3885ee8b8d485c2da08d58f6956567c440db
Those45Ninjas <tom.parker1235@gmail.com>
2019-02-20 22:54:03 +0800

    CREATED A SHOVEL and started a product 'system'

---
+++ 4hr-game/Assets/Scripts/ProductDefinition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Product", menuName = "Create Product")]
+public class ProductDefinition : ScriptableObject
+{
+    // The title of the product.
+    public string Title;
+    // Other names this product could have.
+    public string[] Alias;
+
+    // The price of this product.
+    public float Price;
+
+    // The prefab used for this product.
+    public GameObject Prefab;
+
+    // This will be set by an icon generation script while the game is loading.
+    [HideInInspector]
+    public Sprite Icon;
+}
