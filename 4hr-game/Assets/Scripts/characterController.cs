e3c352556326e846afd7577a9b548288be5f5dd8
alexkirwan29 <alexkirwan29@gmail.com>
2019-02-23 17:11:07 +0800

    fps controller update

--- 4hr-game/Assets/Scripts/characterController.cs
+++ 4hr-game/Assets/Scripts/characterController.cs
@@ -10,15 +10,27 @@ public class characterController : MonoBehaviour
     public float speed = 1.0F;
     public float rotateSpeed = 1.0F;
 
-
-    void Update()
+    private void Update()
     {
+
+
+
         CharacterController controller = GetComponent<CharacterController>();
 
-        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+        float mouseInput = Input.GetAxis("Mouse X");
+        Vector3 lookhere = new Vector3(0, mouseInput, 0);
+        transform.Rotate(lookhere);
 
-        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 forward = transform.forward;
         float curSpeed = speed * Input.GetAxis("Vertical");
         controller.SimpleMove(forward * curSpeed);
+
+        Vector3 sideways = new Vector3(0, 5, 0);
+        float xurSpeed = speed * Input.GetAxis("Horizontal");
+        controller.SimpleMove(sideways * xurSpeed);
+
+
+
+
     }
 }
---
+++ 4hr-game/Assets/Scripts/lookUpDown.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lookUpDown : MonoBehaviour
+{
+
+
+    private void Update()
+    {
+
+        float mouseInput = Input.GetAxis("Mouse Y");
+        Vector3 lookup = new Vector3(-mouseInput, 0, 0);
+        transform.Rotate(lookup);
+
+    }
+
+
+
+}
