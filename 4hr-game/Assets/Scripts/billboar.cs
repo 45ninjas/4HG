067121b080ae409885014840d48e6c526ce5258d
alexkirwan29 <alexkirwan29@gmail.com>
2019-02-24 23:06:56 +0800

    Added sprites for customer emotions

---
+++ 4hr-game/Assets/Scripts/billboar.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class billboar : MonoBehaviour
+{
+
+    public Camera m_Camera; //main camera
+
+    private void LateUpdate()
+    {
+        transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
+            m_Camera.transform.rotation * Vector3.up);
+    }
+}
