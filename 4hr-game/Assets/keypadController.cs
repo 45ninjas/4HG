f0cda65f8d5fcca0a69606ab50683e8c4ce50cd8
alexkirwan29 <alexkirwan29@gmail.com>
2019-02-24 20:10:34 +0800

    started working on pos stuff

---
+++ 4hr-game/Assets/keypadController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class keypadController : MonoBehaviour
+{
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Button[] buttons = this.GetComponentsInChildren<Button>();
+        Debug.Log("buttons.length = " + buttons.Length);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+
+
+    }
+
+
+   
+}
