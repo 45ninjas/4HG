7654bed7ed15d2a80a8fc4e3cc59899da5ebd0a4
Those45Ninjas <tom.parker1235@gmail.com>
2019-03-04 15:29:37 +0800

    Started the IslePlacer proc layer

---
+++ 4hr-game/Assets/Scripts/Proc/Layers/IslePlacer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NineFive.Proc.Layers
+{
+    [CreateAssetMenu(menuName = "Proc Layer/Isle Placer", fileName = "Isle Placer")]
+    public class IslePlacer: ProcLayer
+    {
+        public int Spacing = 2;
+        public GameObject ShelfPrefab;
+
+        Vector2Int direction;
+
+        List<List<Vector2Int>> lines;
+
+        private System.Random random;
+
+        public override void Apply(Store store, System.Random random)
+        {
+            this.random = random;
+            direction = ProcHelper.RandomDirection(random);
+
+            lines = new List<List<Vector2Int>>();
+
+        }
+    }
+}
